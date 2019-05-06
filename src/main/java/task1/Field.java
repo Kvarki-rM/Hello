8b4385063ac647d220cdd1b049f0cdf1393a5140
Kirill <7kreker7@gmail.com>
2019-05-06 19:10:48 +0300

    Initial commit

---
+++ src/main/java/task1/Field.java
@@ -0,0 +1,94 @@
+package task1;
+
+class Field {
+    int x;
+    int y;
+    static int[][] field;
+
+    Field(int h, int w) {
+        this.x = h;
+        this.y = w;
+        field = new int[this.x][this.y];
+    }
+
+    int resultsX() {
+        return (scanner(1));
+    }
+
+    int results0() {
+        return (scanner(2));
+    }
+
+    private static int scanner(int count) {
+        int max = 0;
+        int temp;
+
+        for (int[] ints : field) {
+            for (int j = 0; j < ints.length; j++) {
+                temp = 0;
+                if ((ints[j] == count))
+                    for (int z = 0; z < field[1].length - j; z++)
+                        if (ints[j + z] == count)
+                            temp++;
+                        else break;
+                if (temp > max)
+                    max = temp;
+            }
+        }
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[1].length; j++) {
+                temp = 0;
+                if ((field[i][j] == count))
+                    for (int z = 0; z < field.length - i; z++)
+                        if (field[i + z][j] == count)
+                            temp++;
+                        else break;
+                if (temp > max)
+                    max = temp;
+            }
+        }
+
+        int[][] revert = new int[field.length][field[1].length];
+        for (int i = 0; i < field.length; i++)
+            for (int j = 0; j < field[1].length; j++)
+                revert[i][j] = field[i][field[1].length - j - 1];
+
+        max = diagScanner(field, max, count);
+        max = diagScanner(revert, max, count);
+
+        return max;
+    }
+
+    private static int diagScanner(int[][] field, int max, int num) {
+        int temp;
+        int max2 = max;
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[i].length; j++) {
+                temp = 0;
+                if ((field[i][j] == num))
+                    for (int z = 0; z < Math.min(field[1].length - j, field.length - i); z++)
+                        if (field[i + z][j + z] == num)
+                            temp++;
+                        else break;
+                if (temp > max2)
+                    max2 = temp;
+            }
+        }
+        return max2;
+    }
+
+    static void print() {
+        for (int[] ints : field) {
+            for (int anInt : ints) {
+                System.out.print(anInt + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void add(int value, int column, int line) {
+        field[column][line] = value;
+    }
+
+
+}
---
+++ src/test/java/task1/MainTest.java
@@ -0,0 +1,118 @@
+package task1;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+
+class MainTest {
+    @Test
+    void mainTest() {
+
+        Field first = new Field(6, 8);
+
+        first.x = 6;
+        first.y = 8;
+
+        Field.add(2, 0, 0);
+        Field.add(2, 0, 2);
+        Field.add(2, 0, 3);
+        Field.add(1, 0, 4);
+        Field.add(1, 0, 5);
+        Field.add(2, 0, 6);
+        Field.add(1, 0, 7);
+
+        Field.add(1, 1, 0);
+        Field.add(2, 1, 1);
+        Field.add(1, 1, 2);
+        Field.add(2, 1, 3);
+        Field.add(2, 1, 4);
+        Field.add(2, 1, 5);
+        Field.add(1, 1, 6);
+        Field.add(2, 1, 7);
+
+        Field.add(2, 2, 0);
+        Field.add(2, 2, 2);
+        Field.add(1, 2, 4);
+        Field.add(1, 2, 5);
+        Field.add(1, 2, 7);
+
+        Field.add(2, 3, 0);
+        Field.add(2, 3, 1);
+        Field.add(1, 3, 2);
+        Field.add(2, 3, 3);
+        Field.add(1, 3, 4);
+        Field.add(2, 3, 5);
+        Field.add(1, 3, 6);
+        Field.add(2, 3, 7);
+
+        Field.add(1, 4, 0);
+        Field.add(1, 4, 1);
+        Field.add(2, 4, 2);
+        Field.add(1, 4, 3);
+        Field.add(2, 4, 4);
+        Field.add(1, 4, 5);
+
+        Field.add(2, 5, 2);
+        Field.add(1, 5, 4);
+        Field.add(2, 5, 5);
+        Field.add(1, 5, 7);
+
+        assertEquals(5, first.resultsX());
+        assertEquals(6, first.results0());
+        assertTrue(matrixComparison(Field.field, new int[][]{
+                {2, 0, 2, 2, 1, 1, 2, 1},
+                {1, 2, 1, 2, 2, 2, 1, 2},
+                {2, 0, 2, 0, 1, 1, 0, 1},
+                {2, 2, 1, 2, 1, 2, 1, 2},
+                {1, 1, 2, 1, 2, 1, 0, 0},
+                {0, 0, 2, 0, 1, 2, 0, 1}}));
+
+////////////////////////////////////////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+        Field second = new Field(4, 4);
+
+        second.x = 4;
+        second.y = 4;
+
+        Field.add(1, 0, 0);
+        Field.add(1, 0, 2);
+        Field.add(2, 0, 3);
+
+        Field.add(1, 1, 0);
+        Field.add(1, 1, 1);
+        Field.add(1, 1, 2);
+        Field.add(2, 1, 3);
+
+        Field.add(2, 2, 0);
+        Field.add(2, 2, 1);
+        Field.add(1, 2, 2);
+
+        Field.add(2, 3, 0);
+        Field.add(2, 3, 1);
+        Field.add(1, 3, 3);
+
+        assertEquals(4, second.resultsX());
+        assertEquals(2, second.results0());
+        assertTrue(matrixComparison(Field.field, new int[][]{
+                {1, 0, 1, 2},
+                {1, 1, 1, 2},
+                {2, 2, 1, 0},
+                {2, 2, 0, 1}}));
+    }
+
+    boolean matrixComparison(int[][] a, int[][] b) {
+        if (a.length != b.length || a[0].length != b[0].length) {
+            return false;
+        } else {
+            for (int i = 0; i < a.length; i++)
+                for (int j = 0; j < a[0].length; j++)
+                    if (a[i][j] != b[i][j])
+                        return false;
+        }
+        return true;
+    }
+}
